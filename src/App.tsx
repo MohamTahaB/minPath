6e6bf70deff074b8eb037bff0c0a240f9f8ae61a
MohamedTahaBayoumi <bayoumimt@gmail.com>
2024-02-10 19:24:08 +0100

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
 import { useState } from "react";
 import { BlockTable } from "./components/BlockTable";
-import { InputNumber } from "antd";
+import { InputNumber, Layout } from "antd";
 import { table } from "./logic/searchLogic";
+
+const { Header, Content, Sider } = Layout;
 
 function App() {
     const [size, setSize] = useState<number | null>(4);
     const currentTable = table(size === null ? 3 : size);
     console.log("testing the table gen", currentTable);
     return (
-        <div>
-            <BlockTable size={size === null ? 3 : size} table={currentTable} />
-            <div>
-                <InputNumber min={4} max={10} value={size} onChange={setSize} />
-            </div>
-        </div>
+        <Layout style={{ minHeight: "100vh" }}>
+            <Header> test </Header>
+            <Layout>
+                <Sider>left sidebar</Sider>
+                <Content>
+                    <div>
+                        <BlockTable
+                            size={size === null ? 3 : size}
+                            table={currentTable}
+                        />
+                        <div>
+                            <InputNumber
+                                min={4}
+                                max={10}
+                                value={size}
+                                onChange={setSize}
+                            />
+                        </div>
+                    </div>{" "}
+                </Content>
+            </Layout>
+        </Layout>
     );
 }
 
