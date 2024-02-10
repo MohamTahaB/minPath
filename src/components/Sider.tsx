02c3347f9b5b3683db60120a4f35c1f44a885dde
MohamedTahaBayoumi <bayoumimt@gmail.com>
2024-02-10 20:00:08 +0100

    minor changes

--- src/components/Sider.tsx
+++ src/components/Sider.tsx
@@ -1,10 +1,12 @@
 import { Layout } from "antd"
+import { useState } from "react"
 
 const {Sider: AntdSider} = Layout 
 
 const Sider = () => {
+    const [collapsed, setCollapsed] = useState<boolean>(false)
     return (
-        <AntdSider>
+        <AntdSider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
             left sidebar
         </AntdSider>
     )
